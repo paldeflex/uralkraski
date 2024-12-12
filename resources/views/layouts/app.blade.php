383523de43f7571431383a6bd22699f5387f6553
paldeflex <paldeflex@gmail.com>
2024-12-12 15:13:34 +0500

    Динамическое название, язык, csrf_token для шаблона

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-',  app()->getLocale())  }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title') | Завод Снежинка</title>
+    <title>@yield('title') | {{config('app.name', 'Laravel')}}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 <header>Шапка</header>
